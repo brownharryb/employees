055528b1921953add78e55ac2850888d2e664c08
Boma <bomsy1@gmail.com>
2018-07-24 13:18:54 +0100

    Add Test

--- employee/models.py
+++ employee/models.py
@@ -1,26 +1,6 @@
 from django.db import models
-from django.core import validators
-from django.core.exceptions import ValidationError
-from django.utils.translation import gettext_lazy as _
-import re
-
-
-def alpha_validator():
-	'''Validate only alphabets and hyphens.'''
-	return [validators.RegexValidator("^[a-zA-Z- ]*$",message="Invalid Input!")]
-
-def phone_validator():
-	'''Validate Phone Numbers.'''
-	return [validators.RegexValidator("^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$",
-			message="Invalid Phone Number!")]
-
-def employee_number_validator(emp_number):
-	'''Validate Employee Number to 'EMP-XXXX'.'''
-	if not re.match(r"^EMP-[0-9]{4,9}$",emp_number):
-		raise ValidationError(_('Employee number should be "EMP-XXXX"!'))
-
-
-
+from .validators import (alpha_validator,phone_validator,
+						employee_number_validator)
 
 
 class Employee(models.Model):
--- employee/tests.py
+++ employee/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Employee
+from .validators import employee_number_validator
+from django.core.exceptions import ValidationError
 
-# Create your tests here.
+
+class EmployeeTest(TestCase):
+
+	def test_employee_number_validator(self):
+		'''Employee number matches 'EMP-XXXX'.'''
+		employee_number_validator("EMP-1234")
+		employee_number_validator("EMP-12345")
+		employee_number_validator("EMP-123456")
+		for sample in ('sjkdbj','EMPo-kjbj','EMP-123','EMP-gfd12'):
+			with self.assertRaises(ValidationError):
+				employee_number_validator(sample)
